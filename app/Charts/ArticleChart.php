1a220de76ede383eb00fd7e30fb8cfd0d794a241
candra <candraherdiansyah14@gmail.com>
2022-03-14 09:01:57 +0700

    initial github

--- app/Charts/ArticleChart.php
+++ app/Charts/ArticleChart.php
@@ -1,32 +1,30 @@
 <?php
 
-declare(strict_types = 1);
+declare (strict_types = 1);
 
 namespace App\Charts;
 
+use App\Models\Article;
+use Carbon\Carbon;
 use Chartisan\PHP\Chartisan;
 use ConsoleTVs\Charts\BaseChart;
-use Illuminate\Http\Request;
-use Carbon\Carbon;
 use DB;
-use App\Models\Article;
+use Illuminate\Http\Request;
+
 class ArticleChart extends BaseChart
 {
-    public ?string $name = 'article_chart';
-    public ?string $routeName = 'article_chart';
-    public ?string $prefix = 'admin';
-    public ?array $middlewares = ['auth'];
-
-
+    ?stringpublic $name = 'article_chart';
+    ?stringpublic $routeName = 'article_chart';
+    ?stringpublic $prefix = 'admin';
+    ?arraypublic $middlewares = ['auth'];
 
     /**
      * Handles the HTTP request for the given chart.
      * It must always return an instance of Chartisan
      * and never a string or an array.
      */
-    public function handler(Request $request): Chartisan
+    public function handler(Request $request) : Chartisan
     {
-
 
         $articles = DB::table('articles')->get();
         $labels = [];
@@ -35,11 +33,12 @@ class ArticleChart extends BaseChart
             array_push($labels, $data->created_at);
         }
         $values = Article::select('*')
-                ->whereMonth('created_at', Carbon::now()->month)
-                ->get();
+            ->whereMonth('created_at', Carbon::now()->month)
+            ->get();
         foreach ($values as $item) {
             array_push($count, $item->count());
         }
+        //
         return Chartisan::build()
             ->labels($labels)
             ->dataset('Artikel Rilis', $count);
